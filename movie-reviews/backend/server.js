7f4b370a90bc03876f9b97d0d6b2dda7d643f630
Jude Collins <jude.collins13@yahoo.com>
2022-04-04 20:06:07 -0400

    got server running and pulling routes!

--- movie-reviews/backend/server.js
+++ movie-reviews/backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import movies from './api/movies.routes.js'
+import movies from './api/movies.route.js'
 
 
 const app = express()
@@ -8,9 +8,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use("/api/v1/movies",movies)
+app.use("/api/v1/movies", movies)
 app.use('*', (req,res)=>{
-    res.status(404).json({error: 'Not Found'})
+    res.status(404).json({error: "Not Found"})
 })
 
 export default app
