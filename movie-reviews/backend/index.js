b62eb04648ad0213858d6bd56f8a0f105172cf95
Jude Collins <jude.collins13@yahoo.com>
2022-04-06 20:40:52 -0400

    working on routes for titles rating etc.

--- movie-reviews/backend/index.js
+++ movie-reviews/backend/index.js
@@ -1,32 +1,28 @@
 import app from './server.js'
 import mongodb from "mongodb"
-import dotenv from "dotenv"
-import MoviesDao from './dao/moviesDAO.js'
+import dotenv from "dotenv" 
+import MoviesDAO from './dao/moviesDAO.js' 
+import ReviewsDAO from './dao/reviewsDAO.js' 
 
-async function main (){
+async function main(){        
+ 
+    dotenv.config()    
+    const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI)
+    const port = process.env.PORT || 8000     
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect()
+        await MoviesDAO.injectDB(client)
+        await ReviewsDAO.injectDB(client)
+        app.listen(port, () =>{
+            console.log('server is running on port:'+port);        
+        })
 
-    dotenv.config()
-    
-    const client = new mongodb.MongoClient(
-        process.env.MOVIEREVIEWS_DB_URL
-    )
-    const port = process.env.PORT ||8000
-
-   try { 
-   // connect to the mongodb cluster
-   await client.connect()
-   await MoviesDao.injectDB(client)
-
-   app.listen(port, () =>{
-       console.log('server is running on port:'+port);
-   })
-
-} catch (e) {
-    console.error(e);
-    process.exit(1)
+    } catch (e) {
+        console.error(e);        
+        process.exit(1)
+    } 
 }
-    }
-
 
 main().catch(console.error);
-
